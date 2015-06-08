ab0ddb254159cf607f01e86253e405f40215664e
nwin <nwin@users.noreply.github.com>
2015-06-08 19:18:42 +0200

    Use more of black_box.

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,14 +3,16 @@ extern crate test;
 extern crate byteio;
 extern crate byteorder;
 
+use test::black_box;
+
 #[bench]
 fn bench_byteio_vec(b: &mut test::Bencher) {
     use byteio::ReadBytesExt;
     let vec = vec![0u8; 1_000_000];
     b.iter(|| {
-        let data = test::black_box(&vec[..]);
+        let data = black_box(&vec[..]);
         for mut val in data.chunks(2) {
-            let _: Result<u16, _> = val.read::<byteio::LittleEndian>();
+            let _: Result<u16, _> = black_box(val.read::<byteio::LittleEndian>());
         }
     });
     b.bytes = vec.len() as u64;
@@ -22,8 +24,8 @@ fn bench_byteio(b: &mut test::Bencher) {
     const NITER: i32 = 100_000;
     b.iter(|| {
         for _ in 1..NITER {
-            let data = test::black_box([1, 2]);
-            let _: u16 = byteio::LittleEndian::from_bytes(data);
+            let data = black_box([1, 2]);
+            let _: u16 = black_box(byteio::LittleEndian::from_bytes(data));
         }
     });
     b.bytes = 2 * NITER as u64;
@@ -34,9 +36,9 @@ fn bench_byteorder_vec(b: &mut test::Bencher) {
     use byteorder::ReadBytesExt;
     let vec = vec![0u8; 1_000_000];
     b.iter(|| {
-        let data = test::black_box(&vec[..]);
+        let data = black_box(&vec[..]);
         for mut val in data.chunks(2) {
-            let _: Result<u16, _> = val.read_u16::<byteorder::LittleEndian>();
+            let _: Result<u16, _> = black_box(val.read_u16::<byteorder::LittleEndian>());
         }
     });
     b.bytes = vec.len() as u64;
@@ -48,8 +50,8 @@ fn bench_byteorder(b: &mut test::Bencher) {
     const NITER: i32 = 100_000;
     b.iter(|| {
         for _ in 1..NITER {
-            let data = test::black_box([1, 2]);
-            let _: u16 = byteorder::LittleEndian::read_u16(&data);
+            let data = black_box([1, 2]);
+            let _: u16 = black_box(byteorder::LittleEndian::read_u16(&data));
         }
     });
     b.bytes = 2 * NITER as u64;
